720b4cc03c34221f1dc7334bfa9fae8b2cc175bf
Brandon Wing <brandon.wing245@gmail.com>
2023-11-03 11:47:04 -0600

    Added helpers folder, uuid package, and changed some routes

--- routes/notes.js
+++ routes/notes.js
@@ -1,3 +1,12 @@
 const notes = require("express").Router();
+const { readFromFile, readAndAppend } = require("../helpers/fsUtils");
+const { v4: uuidv4 } = require("uuid");
+
+// GET route for getting all notes
+notes.get("/", (req, res) => {
+  readFromFile("./db/db.json").then((data) => res.json(JSON.parse(data)));
+});
+
+notes.post("/", (req, res) => {});
 
 module.exports = notes;
